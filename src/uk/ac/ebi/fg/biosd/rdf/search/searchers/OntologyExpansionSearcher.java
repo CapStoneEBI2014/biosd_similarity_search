4f8d84af48681e53eb6cac6be092c514db59ed87
AbdulShakur <aabdulshakur@gmail.com>
2014-04-08 17:22:33 -0400

    I was not sure about the decay factor so I commented out the section that could add the decay factor.

--- src/uk/ac/ebi/fg/biosd/rdf/search/searchers/OntologyExpansionSearcher.java
+++ src/uk/ac/ebi/fg/biosd/rdf/search/searchers/OntologyExpansionSearcher.java
@@ -1,20 +1,46 @@
 package uk.ac.ebi.fg.biosd.rdf.search.searchers;
 
-import java.net.URI;
-import java.util.Collections;
-import java.util.Map;
-
 import uk.ac.ebi.fg.biosd.rdf.search.core.KeySearcher;
 import uk.ac.ebi.fg.biosd.rdf.search.core.SearchKey;
 import uk.ac.ebi.fg.biosd.rdf.search.core.SearchResult;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by shakur on 18/03/2014.
  */
 public class OntologyExpansionSearcher extends KeySearcher{
+
+    OntologyTermExpander ontologyTermExpander = new OntologyTermExpander();
+    OntologyKeySearcher ontologyKeySearcher = new OntologyKeySearcher();
+
+    Float decayFactor = 1f;
+
     @Override
     public Map<URI, SearchResult> search(SearchKey key, int offset, int limit) {
-        //TODO: implement calls to OntologyTermExpander and OntologyKeySearcher
-        return Collections.emptyMap();
+
+        Map<URI, SearchResult> globalResults = new HashMap<URI, SearchResult>();
+        if(key.getOntoTermURI() != null) {
+          List<SearchResult> termResults = ontologyTermExpander.getMoreTerms(key.getOntoTermURI(), 1.0);
+
+          for(SearchResult sr: termResults) {
+              Map<URI, SearchResult> keyResults = ontologyKeySearcher.search(new SearchKey(sr.getUri()), offset, limit);
+
+              for(SearchResult kr: keyResults.values()) {
+                  if(globalResults.get(kr.getUri()) == null) {
+                      globalResults.put(kr.getUri(), kr);
+                  } else {
+                      SearchResult res = globalResults.get(kr.getUri());
+//                      res.setScore(res.getScore() + kr.getScore());
+//                      do we need to take care of a decay factor?
+                      res.setScore(res.getScore() + kr.getScore() * decayFactor );
+                  }
+              }
+          }
+        }
+        return globalResults;
     }
 }
